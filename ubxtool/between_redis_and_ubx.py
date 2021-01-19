33dadd4bb2b87ef664705d09c76b6d53cbfdec79
a.kotelnikov <222brain222@gmail.com>
2021-01-19 20:49:30 +0300

    moved ubxtool into thread and implemented parsing and sending to redis of ubxtool responses

--- ubxtool/between_redis_and_ubx.py
+++ ubxtool/between_redis_and_ubx.py
@@ -120,24 +120,26 @@ if __name__ == '__main__':
 
 
 
-
+###############################################
  
-
 import re
 import threading
 import subprocess
+import redis
+redis_client = redis.Redis(host='127.0.0.1',\
+                    port=6379,\
+                    db=3)
 def run(command):
     print(command)
     p = subprocess.Popen(command, shell = True, stdout = subprocess.PIPE)
     (output, err) = p.communicate()
     p_status =  p.wait()
-    print(output)
+    print(output.decode('utf-8'))
     return output
+
 class ubx_to_redis(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
-    def ubx_get_item(self, item):
-        return 'ubxtool -P 27.12 -g {}'.format(item)
     def run(self):
         while True:# gps_thread.running:
             items = ['CFG-NAVSPG-DYNMODEL',\
@@ -150,7 +152,11 @@ class ubx_to_redis(threading.Thread):
             for item in items:
                 print('\n',item, '\n')
                 a = run(self.ubx_get_item(item))
-                print(re.findall('val \d*', a.decode('utf-8')))
+                b = re.findall('UBX-CFG-VALGET:\\n version \d layer \d position \d\\n  layers \(\w*\)\\n    item {}/0x\d* val \d*'.format(item), a.decode('utf-8'))
+                for c in b:
+                    redis_client.hset(item, re.findall('layers \(\w*\)',  c)[0],re.findall('val \d*', c)[0])
+    def ubx_get_item(self, item):
+        return 'ubxtool -P 27.12 -g {}'.format(item)
 
 ubx_to_redis_thread = ubx_to_redis()
 ubx_to_redis_thread.start()
